0e66eb0fd73ce487f07f23e5f4ae1957c7383fc1
Ken Gorab <ken.gorab@gmail.com>
2019-08-30 22:08:12 -0400

    Add typescript definition for disassemble function

--- abra_wasm/ts/abra_wasm.d.ts
+++ abra_wasm/ts/abra_wasm.d.ts
@@ -32,3 +32,20 @@ export function runSync(input: string): Error | any;
  * result. This could result in a runtime error, which will also resolve as a successful Promise
  */
 export function runAsync(input: string): Promise<any>;
+
+export interface DisassembleSuccess {
+    success: true,
+    disassembled: string
+}
+
+export interface DisassembleFailure {
+    success: false,
+    error: Error
+}
+
+export type DisassembleResult = DisassembleSuccess | DisassembleFailure
+
+/**
+ * Compiles the input and returns a stringified disassembly
+ */
+export function disassemble(input: string): DisassembleResult | null;
