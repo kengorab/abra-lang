48fcc01fb681c4e2ea1af181a3db7908971eaabf
Ken Gorab <ken.gorab@gmail.com>
2021-10-15 21:29:07 -0400

    i dunno more debugging, github actions i wish i could exec into you

--- abra_core/src/transpile/gcc.rs
+++ abra_core/src/transpile/gcc.rs
@@ -10,6 +10,30 @@ pub fn gcc<S: AsRef<str>>(dotabra_dir: &PathBuf, src_file: S, out_file: S) -> Re
     let abra_base_path = target_path(&project_root, "abra");
     let libgc_base_path = target_path(&project_root, "libgc");
 
+    println!("/usr/local/bin/clang -v");
+    let output = Command::new("/usr/local/bin/clang")
+        .arg("-v")
+        .output()
+        .unwrap();
+    println!("{}", String::from_utf8(output.stdout).unwrap());
+    println!("{}", String::from_utf8(output.stderr).unwrap());
+
+    println!("/usr/bin/clang -v");
+    let output = Command::new("/usr/bin/clang")
+        .arg("-v")
+        .output()
+        .unwrap();
+    println!("{}", String::from_utf8(output.stdout).unwrap());
+    println!("{}", String::from_utf8(output.stderr).unwrap());
+
+    println!("gcc -v");
+    let output = Command::new("gcc")
+        .arg("-v")
+        .output()
+        .unwrap();
+    println!("{}", String::from_utf8(output.stdout).unwrap());
+    println!("{}", String::from_utf8(output.stderr).unwrap());
+
     let output = Command::new("/usr/local/bin/clang")
         .arg(src_file)
         .arg("-o").arg(out_file)
