9c010643e254bcb0dfd3b09048ec11d89146dfbc
Ken Gorab <ken.gorab@gmail.com>
2024-02-29 20:16:13 -0500

    Adding more tests

--- selfhost_test/src/tests.rs
+++ selfhost_test/src/tests.rs
@@ -343,12 +343,16 @@ fn typechecker_tests() {
         .add_test_vs_txt("typechecker/invocation/generics_given_error_type_mismatch.abra", "typechecker/invocation/generics_given_error_type_mismatch.out")
         // Accessor
         .add_test_vs_txt("typechecker/accessor/accessor.1.abra", "typechecker/accessor/accessor.1.out.json")
+        .add_test_vs_txt("typechecker/accessor/accessor.2.abra", "typechecker/accessor/accessor.2.out.json")
         .add_test_vs_txt("typechecker/accessor/error_unknown_field.abra", "typechecker/accessor/error_unknown_field.out")
         .add_test_vs_txt("typechecker/accessor/error_unknown_field_fn.abra", "typechecker/accessor/error_unknown_field_fn.out")
         .add_test_vs_txt("typechecker/accessor/error_unknown_field_opt_known.abra", "typechecker/accessor/error_unknown_field_opt_known.out")
         .add_test_vs_txt("typechecker/accessor/error_unknown_field_opt_unknown.abra", "typechecker/accessor/error_unknown_field_opt_unknown.out")
         .add_test_vs_txt("typechecker/accessor/error_unknown_field_unit.abra", "typechecker/accessor/error_unknown_field_unit.out")
         .add_test_vs_txt("typechecker/accessor/error_unnecessary_optsafe.abra", "typechecker/accessor/error_unnecessary_optsafe.out")
+        .add_test_vs_txt("typechecker/accessor/error_unknown_field_chain_opt_known.abra", "typechecker/accessor/error_unknown_field_chain_opt_known.out")
+        .add_test_vs_txt("typechecker/accessor/error_unknown_field_chain_opt_unknown.abra", "typechecker/accessor/error_unknown_field_chain_opt_unknown.out")
+
 
         // Type identifiers
         .add_test_vs_txt("typechecker/typeidentifier/error_typearg_unknown.abra", "typechecker/typeidentifier/error_typearg_unknown.out")
@@ -432,6 +436,7 @@ fn typechecker_tests() {
         .add_test_vs_txt("typechecker/funcdecl/default_param_value_call.7.abra", "typechecker/funcdecl/default_param_value_call.7.out.json")
         .add_test_vs_txt("typechecker/funcdecl/default_param_value_call.8.abra", "typechecker/funcdecl/default_param_value_call.8.out.json")
         .add_test_vs_txt("typechecker/funcdecl/default_param_value_call.9.abra", "typechecker/funcdecl/default_param_value_call.9.out.json")
+        .add_test_vs_txt("typechecker/funcdecl/default_param_value_accessor.abra", "typechecker/funcdecl/default_param_value_accessor.out.json")
         .add_test_vs_txt("typechecker/funcdecl/error_self_standalone.abra", "typechecker/funcdecl/error_self_standalone.out")
         // Type declaration
         .add_test_vs_txt("typechecker/typedecl/error_duplicate_field.abra", "typechecker/typedecl/error_duplicate_field.out")
