9e2d86d9c84c08e52a50275cc1f71fc7c6021497
Ken Gorab <ken.gorab@gmail.com>
2024-04-21 20:21:06 -0400

    Selfhost: typechecking prelude (#430)
    * Selfhost: typechecking prelude
    
    Working towards typechecking the existing prelude module prior to
    typechecking the entrypoint module. There's quite a few small things
    that need to happen in order for this to work, and this isn't yet
    working fully.
    
    * Making incremental progress
    
    Getting further and further along with typechecking the prelude module
    using the selfhosted typechecker. Getting generics working properly for
    method calls (especially in cases where the generic value is known to be
    a _generic_ in scope), filling in the prelude structs and using those
    interchangeably with their `TypeKind.Primitive___` counterparts.
    
    * Updating tests to reflect additional modules
    
    * Removing (gigantic) accidentally-committed prelude.out.json file
    
    * Minor cleanups

--- selfhost_test/src/tests.rs
+++ selfhost_test/src/tests.rs
@@ -445,7 +445,6 @@ fn typechecker_tests() {
         .add_test_vs_txt("typechecker/invocation/invocation_method.2.abra", "typechecker/invocation/invocation_method.2.out.json")
         .add_test_vs_txt("typechecker/invocation/generics_error_method.1.abra", "typechecker/invocation/generics_error_method.1.out")
         .add_test_vs_txt("typechecker/invocation/generics_error_method.2.abra", "typechecker/invocation/generics_error_method.2.out")
-        .add_test_vs_txt("typechecker/invocation/invocation_method_generics.1.abra", "typechecker/invocation/invocation_method_generics.1.out.json")
         .add_test_vs_txt("typechecker/invocation/generics_error_field.1.abra", "typechecker/invocation/generics_error_field.1.out")
         .add_test_vs_txt("typechecker/invocation/invocation_method_generics.1.abra", "typechecker/invocation/invocation_method_generics.1.out.json")
         .add_test_vs_txt("typechecker/invocation/invocation_method_generics.2.abra", "typechecker/invocation/invocation_method_generics.2.out.json")
