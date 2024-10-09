dda8e5c93d956a0091c42e288d5bc41a1b5f3b51
Ken Gorab <ken.gorab@gmail.com>
2024-10-08 21:36:54 -0400

    Selfhost: compiling lexer with selfhosted compiler! (#472)
    * Selfhost: compiling lexer with selfhosted compiler!
    
    I successfully compiled the selfhosted lexer (specifically the
    `src/lexer.test.abra` file) and ran it with the test files in
    `test/lexer/` to verify that the output was consistent when compared
    with the output of running `src/lexer.test.abra` as compiled by the
    reference compiler. In order to do this, I needed to refactor the test
    runner a bit (and add some test output files which were previously
    missing since it had just compared the output of the reference
    implementation).
    Also, I uncovered a fair amount of bugs in the selfhosted implementation
    as part of this work, mainly related to variable shadowing within the
    same function. I may need to implement some kind of more intelligent
    variable resolution logic, rather than just assuming that the variable
    will be available when emitting code to access it.
    
    * Selfhost: Compiling shadowed variable names
    
    Within a function scope, a variable's name can be "shadowed" in child
    scopes and it should be treated as a distinct variable from any
    same-named variables in parent scopes. Previously, the selfhosted
    compiler would naively keep track of variables in a function solely by
    name, which of course doesn't allow for the following code, for example:
    ```
    func foo() {
      val a = 1
      if true {
        val a = "hello"
      }
    }
    ```
    Instead, I added a mechanism for tracking and uniquely identifying a
    variable within a function, indexing by the variable's name and
    declaration location. At the time of compilation, every identifier is
    known to be resolved to a variable, so as long as these variables are
    indexed properly (using an incrementing int key to handle duplicates),
    it is pretty easy to properly resolve variables in a function's body.

--- selfhost_test/src/test_utils.rs
+++ selfhost_test/src/test_utils.rs
@@ -1,58 +1,73 @@
-use std::{fs, io};
+use std::fs;
 use std::collections::HashMap;
 use std::env::temp_dir;
-use std::io::BufWriter;
-use std::io::Write;
 use std::path::Path;
 use std::process::Command;
 use assert_cmd::cargo::CommandCargoExt;
-use abra_core::lexer;
-use abra_core::lexer::tokens::Token;
-use abra_core::parser::ast::ModuleId;
 use similar::{TextDiff, ChangeTag};
-use abra_core::common::display_error::DisplayError;
 use abra_core::common::util::{get_project_root, random_string};
 use itertools::{EitherOrBoth, Itertools};
 
 enum TestType {
-    VsRust(&'static str),
     VsTxt(&'static str, &'static str),
 }
 
 pub struct TestRunner {
     runner_name: &'static str,
     bin_path: String,
+    selfhosted_bin_path: Option<String>,
     tests: Vec<TestType>,
 }
 
 impl TestRunner {
     pub fn lexer_test_runner() -> Self {
-        Self::test_runner("lexer", "lexer.test.abra", "lexer_test")
+        Self::test_runner("lexer", "lexer.test.abra", "lexer_test", true)
     }
 
     pub fn parser_test_runner() -> Self {
-        Self::test_runner("parser", "parser.test.abra", "parser_test")
+        Self::test_runner("parser", "parser.test.abra", "parser_test", false)
     }
 
     pub fn typechecker_test_runner() -> Self {
-        Self::test_runner("typechecker", "typechecker.test.abra", "typechecker_test")
+        Self::test_runner("typechecker", "typechecker.test.abra", "typechecker_test", false)
     }
 
     pub fn compiler_test_runner() -> CompilerTestRunner {
         CompilerTestRunner {tests: vec![]}
     }
 
-    pub fn test_runner(runner_name: &'static str, src_file: &str, output_bin_file: &str) -> Self {
-        let bin_path = build_test_runner(src_file, output_bin_file);
-        Self { runner_name, bin_path, tests: vec![] }
+    pub fn test_runner(runner_name: &'static str, src_file: &str, output_bin_file: &str, test_selfhosted: bool) -> Self {
+        let bin_path = build_test_runner(&src_file, &output_bin_file);
+
+        let selfhosted_bin_path = if test_selfhosted {
+            let selfhosted_compiler_bin = build_test_runner("compiler.test.abra", "selfhosted_compiler");
+
+            let project_root = get_project_root().unwrap();
+            let selfhost_dir = project_root.join("selfhost");
+            let src_file = &selfhost_dir.join("src").join(&src_file);
+            let abra_wrapper_script = selfhost_dir.join("abra");
+            let output = Command::new(&abra_wrapper_script)
+                .current_dir(&selfhost_dir)
+                .env("COMPILER_BIN", &selfhosted_compiler_bin)
+                .arg("build")
+                .arg("-o")
+                .arg(&output_bin_file)
+                .arg(&src_file)
+                .output()
+                .unwrap();
+            if !output.stderr.is_empty() {
+                eprintln!("Failed to build test runner '{runner_name}'");
+                eprintln!("  Failed to compile {src_file:?} using selfhosted compiler: {}", String::from_utf8(output.stderr).unwrap());
+                panic!();
+            }
+            Some(selfhost_dir.join("._abra").join(&output_bin_file).to_str().unwrap().to_owned())
+        } else {
+            None
+        };
+        Self { runner_name, bin_path, selfhosted_bin_path, tests: vec![] }
     }
 
-    pub fn add_test_vs_rust(mut self, test_path: &'static str) -> Self {
-        self.tests.push(TestType::VsRust(test_path));
-        self
-    }
-
-    pub fn add_test_vs_txt(mut self, test_path: &'static str, txt_path: &'static str) -> Self {
+    pub fn add_test_vs_file(mut self, test_path: &'static str, txt_path: &'static str) -> Self {
         self.tests.push(TestType::VsTxt(test_path, txt_path));
         self
     }
@@ -65,25 +80,11 @@ impl TestRunner {
         let abra_std_dir = project_root.join("abra_core/std");
         let abra_std_dir = abra_std_dir.to_str().unwrap();
 
-        let Self { runner_name, bin_path, tests } = self;
+        let Self { runner_name, bin_path, selfhosted_bin_path, tests } = self;
 
         let mut failures = vec![];
         for test in tests {
             let (test_path, expected_output) = match test {
-                TestType::VsRust(test_file_path) => {
-                    let test_path = selfhost_dir.join("test").join(test_file_path);
-                    let test_path = test_path.to_str().unwrap().to_string();
-                    println!("Running {runner_name} test (vs rust) {test_path}");
-
-                    let module_id = ModuleId::parse_module_path("./test").unwrap();
-                    let contents = std::fs::read_to_string(&test_path).unwrap();
-                    let rust_output = match lexer::lexer::tokenize(&module_id, &contents) {
-                        Ok(tokens) => tokens_to_json(&tokens).unwrap(),
-                        Err(err) => err.get_message(&test_path, &contents)
-                    };
-
-                    (test_path, rust_output)
-                }
                 TestType::VsTxt(test_file_path, comparison_file_path) => {
                     let test_dir = selfhost_dir.join("test");
                     let test_path = test_dir.join(test_file_path);
@@ -124,7 +125,36 @@ impl TestRunner {
                     };
                     eprint!("  {sign}{change}");
                 }
-                failures.push(test_path);
+                if selfhosted_bin_path.is_some() {
+                    failures.push(format!("{test_path} (not selfhost-compiled)"));
+                } else {
+                    failures.push(test_path.clone());
+                }
+            }
+
+            if let Some(selfhosted_bin_path) = &selfhosted_bin_path {
+                let output = Command::new(&selfhosted_bin_path)
+                    .env("ABRA_HOME", &abra_std_dir)
+                    .arg(&test_path)
+                    .output()
+                    .unwrap();
+                assert!(output.stderr.is_empty(), "Runtime error: {}", String::from_utf8(output.stderr).unwrap());
+                let abra_output = String::from_utf8(output.stdout).unwrap();
+
+                if expected_output != abra_output {
+                    eprintln!("  Difference detected between:");
+                    eprintln!("    (The expected output is the 'old' and abra selfhosted output is the 'new')");
+                    let diff = TextDiff::from_lines(&expected_output, &abra_output);
+                    for change in diff.iter_all_changes() {
+                        let sign = match change.tag() {
+                            ChangeTag::Equal => " ",
+                            ChangeTag::Delete => "-",
+                            ChangeTag::Insert => "+",
+                        };
+                        eprint!("  {sign}{change}");
+                    }
+                    failures.push(format!("{test_path} (selfhost-compiled)"));
+                }
             }
         }
 
@@ -152,14 +182,10 @@ pub struct CompilerTestRunner {
 
 impl CompilerTestRunner {
     pub fn add_test(self, test_path: &'static str) -> Self {
-        // run_test_file_with_args_and_env("process.abra", &["-f", "bar", "--baz", "qux"], &[("FOO", "bar")]);
-        // self.tests.push(CompilerTest { test_path, program_args: &[], env: &[] });
-        // self
         self.add_test_with_args_and_env(test_path, &[], &[])
     }
 
     pub fn add_test_with_args_and_env(mut self, test_path: &'static str, program_args: &'static [&'static str], env: &'static [(&'static str, &'static str)]) -> Self {
-        // run_test_file_with_args_and_env("process.abra", &["-f", "bar", "--baz", "qux"], &[("FOO", "bar")]);
         self.tests.push(CompilerTest { test_path, program_args, env });
         self
     }
@@ -265,115 +291,4 @@ fn build_test_runner(runner_src_file: &str, output_bin_file: &str) -> String {
     println!("...built {}", &runner_bin);
 
     runner_bin
-}
-
-fn tokens_to_json(tokens: &Vec<Token>) -> io::Result<String> {
-    let mut buf = BufWriter::new(Vec::new());
-
-    writeln!(&mut buf, "[")?;
-    let len = tokens.len();
-    for (idx, token) in tokens.iter().enumerate() {
-        writeln!(&mut buf, "  {{")?;
-        let pos = token.get_position();
-        writeln!(&mut buf, "    \"position\": [{}, {}],", pos.line, pos.col)?;
-        writeln!(&mut buf, "    \"kind\": {{")?;
-        match token {
-            Token::Int(_, val) => {
-                writeln!(&mut buf, "      \"name\": \"Int\",")?;
-                writeln!(&mut buf, "      \"value\": {}", val)?;
-            }
-            Token::Float(_, val) => {
-                writeln!(&mut buf, "      \"name\": \"Float\",")?;
-                writeln!(&mut buf, "      \"value\": {}", val)?;
-            }
-            Token::String(_, val) => {
-                writeln!(&mut buf, "      \"name\": \"String\",")?;
-                writeln!(&mut buf, "      \"value\": \"{}\"", val)?;
-            }
-            Token::StringInterp(_, _) => todo!(),
-            Token::Bool(_, val) => {
-                writeln!(&mut buf, "      \"name\": \"Bool\",")?;
-                writeln!(&mut buf, "      \"value\": {}", val)?;
-            }
-            Token::Func(_) => writeln!(&mut buf, "      \"name\": \"Func\"")?,
-            Token::Val(_) => writeln!(&mut buf, "      \"name\": \"Val\"")?,
-            Token::Var(_) => writeln!(&mut buf, "      \"name\": \"Var\"")?,
-            Token::If(_) => writeln!(&mut buf, "      \"name\": \"If\"")?,
-            Token::Else(_) => writeln!(&mut buf, "      \"name\": \"Else\"")?,
-            Token::While(_) => writeln!(&mut buf, "      \"name\": \"While\"")?,
-            Token::Break(_) => writeln!(&mut buf, "      \"name\": \"Break\"")?,
-            Token::Continue(_) => writeln!(&mut buf, "      \"name\": \"Continue\"")?,
-            Token::For(_) => writeln!(&mut buf, "      \"name\": \"For\"")?,
-            Token::In(_) => writeln!(&mut buf, "      \"name\": \"In\"")?,
-            Token::Match(_) => writeln!(&mut buf, "      \"name\": \"Match\"")?,
-            Token::Type(_) => writeln!(&mut buf, "      \"name\": \"Type\"")?,
-            Token::Enum(_) => writeln!(&mut buf, "      \"name\": \"Enum\"")?,
-            Token::Return(_, _) => writeln!(&mut buf, "      \"name\": \"Return\"")?,
-            Token::Readonly(_) => writeln!(&mut buf, "      \"name\": \"Readonly\"")?,
-            Token::Import(_) => writeln!(&mut buf, "      \"name\": \"Import\"")?,
-            Token::Export(_) => writeln!(&mut buf, "      \"name\": \"Export\"")?,
-            Token::From(_) => writeln!(&mut buf, "      \"name\": \"From\"")?,
-            Token::As(_) => writeln!(&mut buf, "      \"name\": \"As\"")?,
-            Token::Try(_) => writeln!(&mut buf, "      \"name\": \"Try\"")?,
-            Token::Ident(_, name) => {
-                writeln!(&mut buf, "      \"name\": \"Ident\",")?;
-                writeln!(&mut buf, "      \"value\": \"{}\"", name)?;
-            }
-            Token::Self_(_) => writeln!(&mut buf, "      \"name\": \"Self\"")?,
-            Token::None(_) => writeln!(&mut buf, "      \"name\": \"None\"")?,
-            Token::Assign(_) => writeln!(&mut buf, "      \"name\": \"Assign\"")?,
-            Token::Plus(_) => writeln!(&mut buf, "      \"name\": \"Plus\"")?,
-            Token::PlusEq(_) => writeln!(&mut buf, "      \"name\": \"PlusEq\"")?,
-            Token::Minus(_) => writeln!(&mut buf, "      \"name\": \"Minus\"")?,
-            Token::MinusEq(_) => writeln!(&mut buf, "      \"name\": \"MinusEq\"")?,
-            Token::Star(_) => writeln!(&mut buf, "      \"name\": \"Star\"")?,
-            Token::StarEq(_) => writeln!(&mut buf, "      \"name\": \"StarEq\"")?,
-            Token::Slash(_) => writeln!(&mut buf, "      \"name\": \"Slash\"")?,
-            Token::SlashEq(_) => writeln!(&mut buf, "      \"name\": \"SlashEq\"")?,
-            Token::Percent(_) => writeln!(&mut buf, "      \"name\": \"Percent\"")?,
-            Token::PercentEq(_) => writeln!(&mut buf, "      \"name\": \"PercentEq\"")?,
-            Token::And(_) => writeln!(&mut buf, "      \"name\": \"And\"")?,
-            Token::AndEq(_) => writeln!(&mut buf, "      \"name\": \"AndEq\"")?,
-            Token::Or(_) => writeln!(&mut buf, "      \"name\": \"Or\"")?,
-            Token::OrEq(_) => writeln!(&mut buf, "      \"name\": \"OrEq\"")?,
-            Token::Caret(_) => writeln!(&mut buf, "      \"name\": \"Caret\"")?,
-            Token::Elvis(_) => writeln!(&mut buf, "      \"name\": \"Elvis\"")?,
-            Token::ElvisEq(_) => writeln!(&mut buf, "      \"name\": \"ElvisEq\"")?,
-            Token::GT(_) => writeln!(&mut buf, "      \"name\": \"GT\"")?,
-            Token::GTE(_) => writeln!(&mut buf, "      \"name\": \"GTE\"")?,
-            Token::LT(_) => writeln!(&mut buf, "      \"name\": \"LT\"")?,
-            Token::LTE(_) => writeln!(&mut buf, "      \"name\": \"LTE\"")?,
-            Token::Eq(_) => writeln!(&mut buf, "      \"name\": \"Eq\"")?,
-            Token::Neq(_) => writeln!(&mut buf, "      \"name\": \"Neq\"")?,
-            Token::Bang(_) => writeln!(&mut buf, "      \"name\": \"Bang\"")?,
-            Token::StarStar(_) =>  writeln!(&mut buf, "      \"name\": \"StarStar\"")?,
-            Token::LParen(_, _) => writeln!(&mut buf, "      \"name\": \"LParen\"")?,
-            Token::RParen(_) => writeln!(&mut buf, "      \"name\": \"RParen\"")?,
-            Token::LBrack(_, _) => writeln!(&mut buf, "      \"name\": \"LBrack\"")?,
-            Token::RBrack(_) => writeln!(&mut buf, "      \"name\": \"RBrack\"")?,
-            Token::LBrace(_) => writeln!(&mut buf, "      \"name\": \"LBrace\"")?,
-            Token::RBrace(_) => writeln!(&mut buf, "      \"name\": \"RBrace\"")?,
-            Token::LBraceHash(_) => writeln!(&mut buf, "      \"name\": \"LBraceHash\"")?,
-            Token::Pipe(_) => writeln!(&mut buf, "      \"name\": \"Pipe\"")?,
-            Token::Colon(_) => writeln!(&mut buf, "      \"name\": \"Colon\"")?,
-            Token::Comma(_) => writeln!(&mut buf, "      \"name\": \"Comma\"")?,
-            Token::Question(_) => writeln!(&mut buf, "      \"name\": \"Question\"")?,
-            Token::Dot(_) => writeln!(&mut buf, "      \"name\": \"Dot\"")?,
-            Token::QuestionDot(_) => writeln!(&mut buf, "      \"name\": \"QuestionDot\"")?,
-            Token::Arrow(_) => writeln!(&mut buf, "      \"name\": \"Arrow\"")?,
-            Token::At(_) => writeln!(&mut buf, "      \"name\": \"At\"")?,
-        }
-        writeln!(&mut buf, "    }}")?;
-        write!(&mut buf, "  }}")?;
-        if idx != len - 1 {
-            writeln!(&mut buf, ",")?;
-        } else {
-            writeln!(&mut buf, "")?;
-        }
-    }
-
-    writeln!(&mut buf, "]")?;
-
-    let bytes = buf.into_inner()?;
-    Ok(String::from_utf8(bytes).unwrap())
 }
